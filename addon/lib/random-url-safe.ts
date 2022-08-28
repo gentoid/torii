0d063ceec63844da274ca5b1e057d761e059d004
Viktor Lazarev <viktor.lazarev@umlaut.com>
2022-08-28 19:57:01 +0200

    Port randomUrlSafe to TS

--- addon/lib/random-url-safe.ts
+++ addon/lib/random-url-safe.ts
@@ -1,8 +1,8 @@
-export default function randomUrlSafe(length) {
+export default function randomUrlSafe(length: number) {
   var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var result = '';
 
-  for (var i = 0; i < length; i++) {
+  for (const _ of Array(length)) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
 
