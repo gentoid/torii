229c24dda935a04bfe575ff31f507aa3813004b4
Viktor Lazarev <viktor.lazarev@umlaut.com>
2022-09-03 15:41:51 +0200

    Fix compile error

--- addon/services/iframe.js
+++ addon/services/iframe.js
@@ -1,26 +1,29 @@
 /* eslint-disable ember/no-mixins, ember/no-classic-classes */
 import Evented from '@ember/object/evented';
 import EmberObject from '@ember/object';
-import UiServiceMixin from 'torii/mixins/ui-service-mixin';
+// import UiServiceMixin from 'torii/mixins/ui-service-mixin';
 
-var Iframe = EmberObject.extend(Evented, UiServiceMixin, {
-  openRemote(url) {
-    this.remote = document.createElement('iframe');
-    this.remote.src = url;
-    this.remote.id = 'torii-iframe';
-    var iframeParent = '.torii-iframe-placeholder';
-    document.querySelector(iframeParent).appendChild(this.remote);
-  },
+var Iframe = EmberObject.extend(
+  Evented,
+  /* UiServiceMixin, */ {
+    openRemote(url) {
+      this.remote = document.createElement('iframe');
+      this.remote.src = url;
+      this.remote.id = 'torii-iframe';
+      var iframeParent = '.torii-iframe-placeholder';
+      document.querySelector(iframeParent).appendChild(this.remote);
+    },
 
-  closeRemote() {
-    this.remote.remove();
-  },
+    closeRemote() {
+      this.remote.remove();
+    },
 
-  pollRemote() {
-    if (document.querySelector('#torii-iframe') === null) {
-      this.trigger('didClose');
-    }
-  },
-});
+    pollRemote() {
+      if (document.querySelector('#torii-iframe') === null) {
+        this.trigger('didClose');
+      }
+    },
+  }
+);
 
 export default Iframe;
